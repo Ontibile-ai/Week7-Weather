fa0b9e8e55075c5820b3d363c99bacfb6b28e8d2
Ontibile-ai <ontmmusi@gmail.com>
2026-07-10 14:20:27 +0200

    added search engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+function updateWeatherInfo(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "cbab4a1ta01fc9fe9f10bf357f41o145";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
-
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+
+  searchCity(searchInputElement.value);
 }
+
+searchCity("Paris");
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
