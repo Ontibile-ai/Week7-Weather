1c06dd1c2d20964c5638ae6e831d25ad76892924
Ontibile-ai <ontmmusi@gmail.com>
2026-07-11 23:04:20 +0200

    icon inserted with js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 function updateWeatherInfo(response) {
-  console.log(response.data);
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = response.data.city;
-
+  let descriptionElement = document.querySelector("#description");
+  console.log(response.data.condition.description);
+  descriptionElement.innerHTML = response.data.condition.description;
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+
+  let timeElement = document.querySelector("#time");
+
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" alt="${response.data.condition.description}" />`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) minutes = `0${minutes}`;
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
